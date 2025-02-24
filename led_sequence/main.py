7168307d680a49699467b683f0ddb310f52ba492
Amir Blum <amirgiraffe@gmail.com>
2025-02-24 22:04:28 +0200

    feat: add timing middleware to debug long responses

--- led_sequence/main.py
+++ led_sequence/main.py
@@ -1,9 +1,22 @@
+import time
 import aiohttp
+from aiohttp import web
 import os
 import logging
 from routes import setup_routes
 
-app = aiohttp.web.Application()
+async def timing_middleware(app, handler):
+    async def middleware_handler(request):
+        start_time = time.monotonic()
+        try:
+            response = await handler(request)
+            return response
+        finally:
+            duration = time.monotonic() - start_time
+            logging.info("Request %s %s took %.6f seconds", request.method, request.path, duration)
+    return middleware_handler
+
+app = aiohttp.web.Application(middlewares=[timing_middleware])
 setup_routes(app)
 
 if __name__ == '__main__':
